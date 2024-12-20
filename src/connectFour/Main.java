d427d808dc72fa5e7304d099b5c1ef691b5f5eb9
Final Project ASD <manzil260405@gmail.com>
2024-12-20 18:07:38 +0700

    memperbaiki main di connectFour

--- src/connectFour/Main.java
+++ src/connectFour/Main.java
@@ -15,23 +15,14 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
+    public static final String TITLE = "Connect Four";
     public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-
-        // Input name for the Human player
-        System.out.print("Enter name for Human player: ");
-        String humanName = scanner.nextLine();
-
-        // Set AI's name automatically to "Computer"
-        String aiName = "Computer";
-
-        // Create a new Board instance
-        Board gameBoard = new Board();
-
-        // Create a new Connect4 game instance with the human player, AI player, and the board
-        Connect4 game = new Connect4(humanName, aiName, gameBoard);
-
-        // Start the game
-        game.playGame();
+        JFrame frame = new JFrame(TITLE);
+        // Set the content-pane of the JFrame to an instance of main JPanel
+        frame.setContentPane(new GameMain());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null); // center the application window
+        frame.setVisible(true);
     }
 }
